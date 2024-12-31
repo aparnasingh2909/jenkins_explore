307a42cb0f18a3b5cdbd1316e71f8963c94596b3
Aparna Singh <aparna.singh02@sap.com>
2024-12-31 11:36:30 +0530

    comment added

--- src/main/java/com/example/jenkinsApp/controller/Controller.java
+++ src/main/java/com/example/jenkinsApp/controller/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+    //default endpoint
     @GetMapping("/home")
     public String home()
     {
